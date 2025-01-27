e832c301f970503b33e73ae8548fe7d6163242db
ttm-SP <s.prabhudev@ttmrail.com.au>
2025-01-28 10:34:05 +1100

    CLEAN - Spaces

--- main.c
+++ main.c
@@ -9,19 +9,18 @@
  * main() function
  * @return 0. int return type required by ANSI/ISO standard.
  */
-int main(void)
+int main( void )
 {
   // Configure LED-pins as outputs
-  nrf_gpio_range_cfg_output(28u, 31u);
+  nrf_gpio_range_cfg_output( 28u, 31u );
 
-  while(1)
+  while ( 1 )
   {
-    nrf_gpio_pin_toggle(28u);
-    nrf_gpio_pin_toggle(29u);
-    nrf_gpio_pin_toggle(30u);
-    nrf_gpio_pin_toggle(31u);
-    
-    for(uint32_t i = 1000000 ; i>0 ; i--);
+    nrf_gpio_pin_toggle( 28u );
+    nrf_gpio_pin_toggle( 29u );
+    nrf_gpio_pin_toggle( 30u );
+    nrf_gpio_pin_toggle( 31u );
+
+    for ( uint32_t i = 1000000; i > 0; i-- );
   }
 }
-
